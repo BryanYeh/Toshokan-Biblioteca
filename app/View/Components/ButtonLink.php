02e4f0df7c7dde5739cd660bb2bb36f373990504
BryanYeh <BryanYeh@users.noreply.github.com>
2021-01-28 19:57:49 -0800

    renamed component

--- app/View/Components/ButtonLink.php
+++ app/View/Components/ButtonLink.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class button-link extends Component
+class ButtonLink extends Component
 {
     /**
      * Create a new component instance.
