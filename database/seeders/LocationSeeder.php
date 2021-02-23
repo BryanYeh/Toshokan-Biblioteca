012b285de634022f0ff56ed91f9540fea97bdfaa
BryanYeh <BryanYeh@users.noreply.github.com>
2021-02-22 22:34:43 -0800

    added location seeder

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory;
+
+class LocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < $faker->numberBetween(1, 10); $i++) {
+            DB::table('locations')->insert([
+                'name' => $faker->unique()->company(),
+                'address1' => $faker->streetAddress(),
+                'address2' => $faker->randomElement([$faker->secondaryAddress(),null]),
+                'city' => $faker->city(),
+                'state' => $faker->state(),
+                'postal_code' => $faker->postcode(),
+                'country' => $faker->country(),
+                'phone' => $faker->phoneNumber(),
+            ]);
+        }
+    }
+}
