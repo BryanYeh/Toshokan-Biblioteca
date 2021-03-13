dfb23f235497c10a7e3a65c4571b6e27ec7324d4
BryanYeh <BryanYeh@users.noreply.github.com>
2021-03-12 21:50:58 -0800

    added return book functionality

--- app/Http/Controllers/Admin/LendController.php
+++ app/Http/Controllers/Admin/LendController.php
@@ -12,16 +12,19 @@ use Carbon\Carbon;
 
 class LendController extends Controller
 {
+    // lend book page
     public function __invoke()
     {
         return Inertia::render('Admin/Lend/View');
     }
 
+    // load patron by card
     public function load(Request $request)
     {
         return Inertia::render('Admin/Lend/View',User::where('card_number',$request->card_number)->firstOrFail());
     }
 
+    // checkout book to patron
     public function checkout(Request $request)
     {
         $user = User::where('card_number',$request->card_number)->first();
@@ -34,13 +37,22 @@ class LendController extends Controller
         return Inertia::render('Admin/Lend/View', $book );
     }
 
+    // return book page
     public function checkinView()
     {
         return Inertia::render('Admin/Lend/Checkin');
     }
 
-    public function checkin()
+    // return book
+    public function checkin(Request $request)
     {
+        $bookLocation = BookLocation::where('barcode', $request->barcode)->firstOrFail();
+        $lend = Lend::where('book_id',$bookLocation->id)->whereNull('returned_date')->firstOrFail();
+        $lend->returned_date = Carbon::now();
+        $lend->is_damaged = isset($request->is_damaged);
+        $lend->notes = $request->notes;
+        $lend->save();
+
         return redirect()->back()
             ->with('message', "Book has been returned!");
     }
