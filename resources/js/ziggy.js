28f4cc7ca928ad342b0e4bfb423fa3af1046ff22
BryanYeh <BryanYeh@users.noreply.github.com>
2021-03-20 14:52:22 -0700

    removed ziggy.js

--- resources/js/ziggy.js
+++
@@ -1,9 +0,0 @@
-const Ziggy = {"url":"https:\/\/library.test","port":null,"defaults":{},"routes":{"login":{"uri":"login","methods":["GET","HEAD"]},"logout":{"uri":"logout","methods":["POST"]},"password.request":{"uri":"forgot-password","methods":["GET","HEAD"]},"password.reset":{"uri":"reset-password\/{token}","methods":["GET","HEAD"]},"password.email":{"uri":"forgot-password","methods":["POST"]},"password.update":{"uri":"reset-password","methods":["POST"]},"register":{"uri":"register","methods":["GET","HEAD"]},"user-profile-information.update":{"uri":"user\/profile-information","methods":["PUT"]},"user-password.update":{"uri":"user\/password","methods":["PUT"]},"password.confirm":{"uri":"user\/confirm-password","methods":["GET","HEAD"]},"password.confirmation":{"uri":"user\/confirmed-password-status","methods":["GET","HEAD"]},"two-factor.login":{"uri":"two-factor-challenge","methods":["GET","HEAD"]},"profile.show":{"uri":"user\/profile","methods":["GET","HEAD"]},"other-browser-sessions.destroy":{"uri":"user\/other-browser-sessions","methods":["DELETE"]},"current-user-photo.destroy":{"uri":"user\/profile-photo","methods":["DELETE"]},"current-user.destroy":{"uri":"user","methods":["DELETE"]},"dashboard":{"uri":"dashboard","methods":["GET","HEAD"]},"invite":{"uri":"invite","methods":["GET","HEAD"]},"inviteLibrarian":{"uri":"invite","methods":["POST"]},"invitation":{"uri":"invitation\/{code}\/email\/{email}","methods":["GET","HEAD"]},"accept":{"uri":"invitation\/accept","methods":["POST"]},"view":{"uri":"librarians","methods":["GET","HEAD"]},"edit.librarian":{"uri":"librarian\/edit\/{id}","methods":["GET","HEAD"]}}};
-
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    for (let name in window.Ziggy.routes) {
-        Ziggy.routes[name] = window.Ziggy.routes[name];
-    }
-}
-
-export { Ziggy };
