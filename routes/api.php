5a0bed38b0fffe138d6525cebc81f779dcfd7797
BryanYeh <BryanYeh@users.noreply.github.com>
2021-03-20 16:56:09 -0700

    move web routes into api routes, modification might be needed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\LibrariansController;
+use App\Http\Controllers\Admin\PatronsController;
+use App\Http\Controllers\Admin\VisitorsController;
+use App\Http\Controllers\Admin\BooksController;
+use App\Http\Controllers\Admin\LocationController;
+use App\Http\Controllers\Admin\LendController;
+use App\Http\Controllers\Admin\InvitationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +20,49 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/invitation/{code}/email/{email}', InvitationController::class)
+    ->whereAlphaNumeric('code')
+    ->where('email','^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$')
+    ->middleware(['guest'])
+    ->name('invitation');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/invitation/accept',[InvitationController::class,'accept'])
+    ->middleware(['guest'])
+    ->name('accept');
+
+Route::get('/librarians', LibrariansController::class)->name('librarians');
+Route::get('/librarians/profile/{id}', [LibrariansController::class,'view'])->name('librarian.profile');
+Route::delete('/librarians/delete/{id}', [LibrariansController::class,'remove'])->name('librarian.delete');
+Route::get('/invite', [LibrariansController::class,'invite'])->name('librarian.invitation');
+Route::post('/invite', [LibrariansController::class,'send'])->name('librarian.invitation.send');
+
+Route::get('/patrons', PatronsController::class)->name('patrons');
+Route::get('/patrons/profile/{id}', [PatronsController::class,'view'])->name('patron.profile');
+Route::get('/patrons/edit/{id}', [PatronsController::class,'edit'])->name('patron.edit');
+Route::post('/patrons/edit/{id}', [PatronsController::class,'update'])->name('patron.update');
+Route::post('/patrons/downgrade/{id}', [PatronsController::class,'downgrade'])->name('patron.downgrade');
+
+Route::get('/visitors', VisitorsController::class)->name('visitors');
+Route::get('/visitors/profile/{id}', [VisitorsController::class,'view'])->name('visitor.profile');
+Route::get('/visitors/upgrade/{id}', [VisitorsController::class,'edit'])->name('visitor.edit');
+Route::post('/visitors/upgrade/{id}', [VisitorsController::class,'upgrade'])->name('visitor.upgrade');
+
+Route::get('/books', BooksController::class)->name('books');
+Route::get('/books/details/{id}', [BooksController::class,'view'])->name('book.detail');
+Route::get('/books/edit/{id}', [BooksController::class,'edit'])->name('book.edit');
+Route::post('/books/update/{id}', [BooksController::class,'update'])->name('book.update');
+Route::get('/books/create', [BooksController::class,'create'])->name('book.create');
+Route::post('/books/create', [BooksController::class,'save'])->name('book.save');
+
+Route::get('/locations', LocationController::class)->name('locations');
+Route::get('/locations/details/{id}', [LocationController::class,'view'])->name('location.detail');
+Route::get('/locations/edit/{id}', [BooksLocationControllerController::class,'edit'])->name('location.edit');
+Route::post('/locations/update/{id}', [LocationController::class,'update'])->name('location.update');
+Route::get('/locations/create', [LocationController::class,'create'])->name('location.create');
+Route::post('/locations/create', [LocationController::class,'save'])->name('location.save');
+
+Route::get('/lend', LendController::class)->name('lend');
+Route::post('/lend/patron', [LendController::class,'load'])->name('lend.load');
+Route::post('/lend/book',[LendController::class,'checkout'])->name('lend.book');
+Route::get('/lend/book/return',[LendController::class,'checkinView'])->name('lend.return.view');
+Route::post('/lend/book/return',[LendController::class,'checkin'])->name('lend.return');
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\InvitationController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\LibrariansController;
-use App\Http\Controllers\Admin\PatronsController;
-use App\Http\Controllers\Admin\VisitorsController;
-use App\Http\Controllers\Admin\BooksController;
-use App\Http\Controllers\Admin\LocationController;
-use App\Http\Controllers\Admin\LendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,55 +12,7 @@ use App\Http\Controllers\Admin\LendController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/invitation/{code}/email/{email}', InvitationController::class)
-    ->whereAlphaNumeric('code')
-    ->where('email','^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$')
-    ->middleware(['guest'])
-    ->name('invitation');
-
-Route::post('/invitation/accept',[InvitationController::class,'accept'])
-    ->middleware(['guest'])
-    ->name('accept');
-
-
-
-Route::get('/librarians', LibrariansController::class)->name('librarians');
-Route::get('/librarians/profile/{id}', [LibrariansController::class,'view'])->name('librarian.profile');
-Route::delete('/librarians/delete/{id}', [LibrariansController::class,'remove'])->name('librarian.delete');
-Route::get('/invite', [LibrariansController::class,'invite'])->name('librarian.invitation');
-Route::post('/invite', [LibrariansController::class,'send'])->name('librarian.invitation.send');
-
-Route::get('/patrons', PatronsController::class)->name('patrons');
-Route::get('/patrons/profile/{id}', [PatronsController::class,'view'])->name('patron.profile');
-Route::get('/patrons/edit/{id}', [PatronsController::class,'edit'])->name('patron.edit');
-Route::post('/patrons/edit/{id}', [PatronsController::class,'update'])->name('patron.update');
-Route::post('/patrons/downgrade/{id}', [PatronsController::class,'downgrade'])->name('patron.downgrade');
-
-Route::get('/visitors', VisitorsController::class)->name('visitors');
-Route::get('/visitors/profile/{id}', [VisitorsController::class,'view'])->name('visitor.profile');
-Route::get('/visitors/upgrade/{id}', [VisitorsController::class,'edit'])->name('visitor.edit');
-Route::post('/visitors/upgrade/{id}', [VisitorsController::class,'upgrade'])->name('visitor.upgrade');
-
-Route::get('/books', BooksController::class)->name('books');
-Route::get('/books/details/{id}', [BooksController::class,'view'])->name('book.detail');
-Route::get('/books/edit/{id}', [BooksController::class,'edit'])->name('book.edit');
-Route::post('/books/update/{id}', [BooksController::class,'update'])->name('book.update');
-Route::get('/books/create', [BooksController::class,'create'])->name('book.create');
-Route::post('/books/create', [BooksController::class,'save'])->name('book.save');
-
-Route::get('/locations', LocationController::class)->name('locations');
-Route::get('/locations/details/{id}', [LocationController::class,'view'])->name('location.detail');
-Route::get('/locations/edit/{id}', [BooksLocationControllerController::class,'edit'])->name('location.edit');
-Route::post('/locations/update/{id}', [LocationController::class,'update'])->name('location.update');
-Route::get('/locations/create', [LocationController::class,'create'])->name('location.create');
-Route::post('/locations/create', [LocationController::class,'save'])->name('location.save');
-
-Route::get('/lend', LendController::class)->name('lend');
-Route::post('/lend/patron', [LendController::class,'load'])->name('lend.load');
-Route::post('/lend/book',[LendController::class,'checkout'])->name('lend.book');
-Route::get('/lend/book/return',[LendController::class,'checkinView'])->name('lend.return.view');
-Route::post('/lend/book/return',[LendController::class,'checkin'])->name('lend.return');
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '.*');
