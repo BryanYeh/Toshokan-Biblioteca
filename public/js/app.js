3ea8f476f7780eb346098f3b64681ab5ed9a1254
BryanYeh <bbryanyeh@gmail.com>
2021-07-18 21:39:52 -0700

    add: visitor profile page

--- public/js/app.js
+++ public/js/app.js
@@ -17430,7 +17430,30 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  props: {
+    uuid: {
+      required: true
+    }
+  },
+  data: function data() {
+    return {
+      visitor: {}
+    };
+  },
+  methods: {
+    getUser: function getUser() {
+      var _this = this;
+
+      axios.get("/api/admin/visitor/" + this.uuid).then(function (response) {
+        _this.visitor = response.data;
+      });
+    }
+  },
+  created: function created() {
+    this.getUser();
+  }
+});
 
 /***/ }),
 
@@ -18832,8 +18855,57 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "render": () => (/* binding */ render)
 /* harmony export */ });
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
+
+
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", {
+  "class": "w-full mt-6"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("button", {
+  "class": "px-4 py-2 bg-yellow-100 text-yellow-500 rounded-md shadow-md hover:bg-yellow-500 hover:text-white"
+}, "Upgrade")], -1
+/* HOISTED */
+);
+
+var _hoisted_2 = {
+  "class": "w-full rounded-md shadow-md bg-white mt-4 p-4"
+};
+
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("span", {
+  "class": "font-semibold"
+}, "First Name", -1
+/* HOISTED */
+);
+
+var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("span", {
+  "class": "font-semibold"
+}, "Last Name", -1
+/* HOISTED */
+);
+
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("span", {
+  "class": "font-semibold"
+}, "Email", -1
+/* HOISTED */
+);
+
+var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("span", {
+  "class": "font-semibold"
+}, "Username", -1
+/* HOISTED */
+);
+
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return " Hello from visitor profile page ";
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [_hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("p", null, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(": " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.visitor.first_name), 1
+  /* TEXT */
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("p", null, [_hoisted_4, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(": " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.visitor.last_name), 1
+  /* TEXT */
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("p", null, [_hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(": " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.visitor.email), 1
+  /* TEXT */
+  )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("p", null, [_hoisted_6, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(": " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.visitor.username), 1
+  /* TEXT */
+  )])])], 64
+  /* STABLE_FRAGMENT */
+  );
 }
 
 /***/ }),
--- resources/js/pages/Admin/Visitor/VisitorProfile.vue
+++ resources/js/pages/Admin/Visitor/VisitorProfile.vue
@@ -1,13 +1,34 @@
 <template>
-  Hello from visitor profile page
+    <div class="w-full mt-6">
+        <button class="px-4 py-2 bg-yellow-100 text-yellow-500 rounded-md shadow-md hover:bg-yellow-500 hover:text-white">Upgrade</button>
+    </div>
+    <div class="w-full rounded-md shadow-md bg-white mt-4 p-4">
+        <p><span class="font-semibold">First Name</span>: {{ visitor.first_name }}</p>
+        <p><span class="font-semibold">Last Name</span>: {{ visitor.last_name }}</p>
+        <p><span class="font-semibold">Email</span>: {{ visitor.email }}</p>
+        <p><span class="font-semibold">Username</span>: {{ visitor.username }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        uuid: { required: true },
+    },
+    data() {
+        return {
+            visitor: {}
+        }
+    },
+    methods: {
+        getUser() {
+            axios.get("/api/admin/visitor/" + this.uuid).then((response) => {
+                this.visitor = response.data
+            });
+        },
+    },
+    created() {
+        this.getUser()
+    }
 }
 </script>
-
-<style>
-
-</style>
