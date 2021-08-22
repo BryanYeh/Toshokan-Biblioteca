9087410be3ca36508939c542cd3b643beb4985e3
BryanYeh <bbryanyeh@gmail.com>
2021-08-21 21:57:16 -0700

    update: routes param hashid to id

--- routes/api/v1/admin.php
+++ routes/api/v1/admin.php
@@ -12,48 +12,48 @@ use App\Http\Controllers\Admin\InvitationController;
 Route::group(['prefix' => 'librarians'], function () {
     Route::get('/', LibrariansController::class);
     Route::post('/', [LibrariansController::class,'store']);
-    Route::get('{hashid}', [LibrariansController::class,'show']);
-    Route::put('{hashid}', [LibrariansController::class,'update']);
-    Route::delete('{hashid}', [LibrariansController::class,'destroy']);
+    Route::get('{id}', [LibrariansController::class,'show']);
+    Route::put('{id}', [LibrariansController::class,'update']);
+    Route::delete('{id}', [LibrariansController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'books'], function () {
     Route::get('/', BooksController::class);
     Route::post('/', [BooksController::class,'store']);
-    Route::get('{hashid}', [BooksController::class,'show']);
-    Route::put('{hashid}', [BooksController::class,'update']);
-    Route::delete('{hashid}', [BooksController::class,'destroy']);
+    Route::get('{id}', [BooksController::class,'show']);
+    Route::put('{id}', [BooksController::class,'update']);
+    Route::delete('{id}', [BooksController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'subjects'], function () {
     Route::get('/', SubjectsController::class);
     Route::post('/', [SubjectsController::class,'store']);
-    Route::get('{hashid}', [SubjectsController::class,'show']);
-    Route::put('{hashid}', [SubjectsController::class,'update']);
-    Route::delete('{hashid}', [SubjectsController::class,'destroy']);
+    Route::get('{id}', [SubjectsController::class,'show']);
+    Route::put('{id}', [SubjectsController::class,'update']);
+    Route::delete('{id}', [SubjectsController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'locations'], function () {
     Route::get('/', LocationsController::class);
     Route::post('/', [LocationsController::class,'store']);
-    Route::get('{hashid}', [LocationsController::class,'show']);
-    Route::put('{hashid}', [LocationsController::class,'update']);
-    Route::delete('{hashid}', [LocationsController::class,'destroy']);
+    Route::get('{id}', [LocationsController::class,'show']);
+    Route::put('{id}', [LocationsController::class,'update']);
+    Route::delete('{id}', [LocationsController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'patrons'], function () {
     Route::get('/', PatronsController::class);
     Route::post('/', [PatronsController::class,'store']);
-    Route::get('{hashid}', [PatronsController::class,'show']);
-    Route::put('{hashid}', [PatronsController::class,'update']);
-    Route::delete('{hashid}', [PatronsController::class,'destroy']);
+    Route::get('{id}', [PatronsController::class,'show']);
+    Route::put('{id}', [PatronsController::class,'update']);
+    Route::delete('{id}', [PatronsController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'visitors'], function(){
     Route::get('/', VisitorsController::class);
-    Route::get('{hashid}', [VisitorsController::class,'show']);
-    Route::put('{hashid}', [VisitorsController::class,'update']);
-    Route::delete('{hashid}', [VisitorsController::class,'destroy']);
+    Route::get('{id}', [VisitorsController::class,'show']);
+    Route::put('{id}', [VisitorsController::class,'update']);
+    Route::delete('{id}', [VisitorsController::class,'destroy']);
 });
 
 Route::group(['prefix' => 'invitations'], function () {
