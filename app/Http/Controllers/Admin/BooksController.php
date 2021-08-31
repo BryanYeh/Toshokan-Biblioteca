29438f75ff88d6ade355af7b9e200a04bcd4e458
BryanYeh <bbryanyeh@gmail.com>
2021-08-30 22:03:00 -0700

    update: book update function

--- app/Http/Controllers/Admin/BooksController.php
+++ app/Http/Controllers/Admin/BooksController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Books;
 use App\Models\Location;
 use App\Models\BookLocation;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class BooksController extends Controller
@@ -54,63 +56,83 @@ class BooksController extends Controller
     }
 
     // update book
+    // method: post, field: _method=PUT
     public function update(Request $request)
     {
         $request->validate([
-            'title' => 'required|string',
-            'isbn' => 'required|string',
-            'edition' => 'string|nullable',
-            'summary' => 'required|string',
-            'language' => 'string|nullable',
-            'author' => 'required|string',
-            'publisher' => 'required|string',
+            'title' => 'required',
+            'slug' => 'required',
+            'isbn' => 'required',
+            'edition' => 'nullable',
+            'summary' => 'required',
+            'language' => 'nullable',
+            'image' => 'nullable|mimes:jpg,bmp,png|max:10240',
+            'author' => 'required',
+            'publisher' => 'required',
             'publication_date' => 'required|date',
-            'dewey_decimal' => 'string|nullable',
-            'locations.*.id' => 'integer|nullable',
-            'locations.*.location_id' => 'exists:locations,id|nullable',
-            'locations.*.barcode' => 'string|nullable',
-            'locations.*.call_number' => 'string|nullable',
-            'locations.*.location' => 'string|nullable',
-            'locations.*.price' => 'regex:/^\d+(\.\d{1,2})?$/|nullable'
+            'dewey_decimal' => 'nullable',
+            'locations.*.id' => 'nullable',
+            'locations.*.location_id' => 'exists:locations,id|required',
+            'locations.*.barcode' => 'required_if:locations.*.location_id,null',
+            'locations.*.call_number' => 'required_if:locations.*.location_id,null',
+            'locations.*.section' => 'required_if:locations.*.location_id,null',
+            'locations.*.price' => 'regex:/^\d+(\.\d{1,2})?$/|required_if:locations.*.location_id,null',
+            'subjects.*.subject_id' => 'exists:subjects,id|required',
         ]);
+
         $book = Books::where('id', $request->id)->firstOrFail();
         $book->title = $request->title;
+        $book->slug = Str::slug($request->slug, '_');
         $book->isbn = $request->isbn;
         $book->edition = $request->edition;
         $book->summary = $request->summary;
         $book->language = $request->language;
+
+        $old_image = ''; // save old image to delete AFTER updating book info
+        if($request->hasFile('image')) {
+            $old_image = $book->image;
+            $book->image = $request->file('image')->store('images/covers', 'public');
+        }
+
         $book->author = $request->author;
         $book->publisher = $request->publisher;
         $book->publication_date = $request->publication_date;
         $book->dewey_decimal = $request->dewey_decimal;
         $book->save();
 
-        foreach ($request->locations as $location) {
-            $bookLocation = null;
-            if (isset($location['id']) && !is_null($location['id'])) {
-                $bookLocation = BookLocation::where('id', $location['id'])->first();
-            } elseif (
-                !isset($location['id'])
-                && !is_null($location['location_id'])
-                && !is_null($location['barcode'])
-                && !is_null($location['call_number'])
-                && !is_null($location['location'])
-                && !is_null($location['price'])
-            ) {
-                $bookLocation = new BookLocation();
-                $bookLocation->book_id = $request->id;
-            }
-
-            $bookLocation->location_id = $location['location_id'];
-            $bookLocation->barcode = $location['barcode'];
-            $bookLocation->call_number = $location['call_number'];
-            $bookLocation->location = $location['location'];
-            $bookLocation->price = $location['price'];
-            $bookLocation->save();
+        // delete old book image
+        if($old_image != '') {
+            Storage::disk('public')->delete($old_image);
         }
 
-        $book = Books::where('id', $request->id)->with('locations')->firstOrFail();
-        return $book;
+        foreach ($request->locations as $location) {
+            $copy = BookLocation::where('id', $location['id'])->first();
+            if($copy) {
+                $copy->location_id = $location['location_id'];
+                $copy->barcode = $location['barcode'];
+                $copy->call_number = $location['call_number'];
+                $copy->section = $location['section'];
+                $copy->price = $location['price'];
+                $copy->save();
+            }
+            else {
+                $book
+                    ->locations()
+                    ->attach(
+                        $location['location_id'],
+                        [
+                            'barcode' => $location['barcode'],
+                            'call_number' => $location['call_number'],
+                            'section' => $location['section'],
+                            'price' => $location['price']
+                        ]
+                    );
+            }
+        }
+
+        $book->subjects()->sync($request->subjects);
+
+        return response()->json(['message' => 'Successfully updated book!']);
     }
 
     // create book
@@ -166,6 +188,6 @@ class BooksController extends Controller
 
         $book->subjects()->sync($request->subjects);
 
-        return response()->json(['message' => 'Successfully created book!']);
+        return response()->json(['message' => 'Successfully created book!', 'id' => $book->id ]);
     }
 }
