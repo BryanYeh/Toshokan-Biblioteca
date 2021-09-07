6bda946d5a034a4b8a201dfe6206341e7adf9d6c
BryanYeh <bbryanyeh@gmail.com>
2021-09-06 22:17:59 -0700

    remove: vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,1 @@
 require("./bootstrap");
-
-import { createApp } from "vue";
-import routes from "./router/index";
-import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
-
-createApp(App).use(router).mount("#app");
