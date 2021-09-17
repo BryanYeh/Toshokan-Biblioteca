5fe6126ad880b971b3324f79420b5908180e81ec
BryanYeh <bbryanyeh@gmail.com>
2021-09-16 21:02:58 -0700

    add: passport for oauth2

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Carbon;
+use Laravel\Passport\HasApiTokens;
 
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     /**
      * The attributes that are mass assignable.
      *
